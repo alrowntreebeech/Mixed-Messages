f65a7e6dec180ae8dfc774c90e780bdb7ce94ee5
Alex Rowntree-Beech <118824878+alrowntreebeech@users.noreply.github.com>
2023-01-17 09:35:57 +0000

    Add Type Picutres

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -30,30 +30,204 @@ const capitaliseFirstLetter = (pokemonName) => {
 let pokemonType1 = ["Normal", "Fire", "Water", "Electricity", "Grass", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"];
 let pokemonType2 = ["Normal", "Fire", "Water", "Electricity", "Grass", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy", "None"];
 
+
+//Variable to hold type - to help reset pictures
+
+let type1;
+let type2;
+
+//Reset Picture Type
+
+const resetPicture = () => {
+
+    //Type1 Reset
+    if (type1 === "Normal") {
+        document.getElementById('normal1').style.display = 'none';
+    } else if (type1 === "Bug") {
+        document.getElementById('bug1').style.display = 'none';
+    } else if (type1 === "Dark") {
+        document.getElementById('dark1').style.display = 'none';
+    } else if (type1 === "Dragon") {
+        document.getElementById('dragon1').style.display = 'none';
+    } else if (type1 === "Electricity") {
+        document.getElementById('electric1').style.display = 'none';
+    } else if (type1 === "Fairy") {
+        document.getElementById('fairy1').style.display = 'none';
+    } else if (type1 === "Fighting") {
+        document.getElementById('fighting1').style.display = 'none';
+    } else if (type1 === "Fire") {
+        document.getElementById('fire1').style.display = 'none';
+    } else if (type1 === "Flying") {
+        document.getElementById('flying1').style.display = 'none';
+    } else if (type1 === "Ghost") {
+        document.getElementById('ghost1').style.display = 'none';
+    } else if (type1 === "Grass") {
+        document.getElementById('grass1').style.display = 'none';
+    } else if (type1 === "Ground") {
+        document.getElementById('ground1').style.display = 'none';
+    } else if (type1 === "Ice") {
+        document.getElementById('ice1').style.display = 'none';
+    } else if (type1 === "Poison") {
+        document.getElementById('poison1').style.display = 'none';
+    } else if (type1 === "Psychic") {
+        document.getElementById('psychic1').style.display = 'none';
+    } else if (type1 === "Rock") {
+        document.getElementById('rock1').style.display = 'none';
+    } else if (type1 === "Steel") {
+        document.getElementById('steel1').style.display = 'none';
+    } else if (type1 === "Water") {
+        document.getElementById('water1').style.display = 'none';
+    }
+
+    //Type2 Reset
+    if (type2 === "Normal") {
+        document.getElementById('normal2').style.display = 'none';
+    } else if (type2 === "Bug") {
+        document.getElementById('bug2').style.display = 'none';
+    } else if (type2 === "Dark") {
+        document.getElementById('dark2').style.display = 'none';
+    } else if (type2 === "Dragon") {
+        document.getElementById('dragon2').style.display = 'none';
+    } else if (type2 === "Electricity") {
+        document.getElementById('electric2').style.display = 'none';
+    } else if (type2 === "Fairy") {
+        document.getElementById('fairy2').style.display = 'none';
+    } else if (type2 === "Fighting") {
+        document.getElementById('fighting2').style.display = 'none';
+    } else if (type2 === "Fire") {
+        document.getElementById('fire2').style.display = 'none';
+    } else if (type2 === "Flying") {
+        document.getElementById('flying2').style.display = 'none';
+    } else if (type2 === "Ghost") {
+        document.getElementById('ghost2').style.display = 'none';
+    } else if (type2 === "Grass") {
+        document.getElementById('grass2').style.display = 'none';
+    } else if (type2 === "Ground") {
+        document.getElementById('ground2').style.display = 'none';
+    } else if (type2 === "Ice") {
+        document.getElementById('ice2').style.display = 'none';
+    } else if (type2 === "Poison") {
+        document.getElementById('poison2').style.display = 'none';
+    } else if (type2 === "Psychic") {
+        document.getElementById('psychic2').style.display = 'none';
+    } else if (type2 === "Rock") {
+        document.getElementById('rock2').style.display = 'none';
+    } else if (type2 === "Steel") {
+        document.getElementById('steel2').style.display = 'none';
+    } else if (type2 === "Water") {
+        document.getElementById('water2').style.display= 'none';
+    } 
+}
+
 //Second Pokemon Type - declaring first and then comparing with first type to make sure there are not two of the same types
 const secondType = (type1) => {
-
-    let type2;
+    
     do{
     let arrayPos = Math.floor(Math.random() * 19);
     type2 = pokemonType2[arrayPos];
     } while (type2 === type1);
     document.getElementById('type2').innerHTML = type2;
+
+    //Pairing Type Symbol with Type
+    if (type2 === "Normal") {
+        document.getElementById('normal2').style.display = 'block';
+    } else if (type2 === "Bug") {
+        document.getElementById('bug2').style.display = 'block';
+    } else if (type2 === "Dark") {
+        document.getElementById('dark2').style.display = 'block';
+    } else if (type2 === "Dragon") {
+        document.getElementById('dragon2').style.display = 'block';
+    } else if (type2 === "Electricity") {
+        document.getElementById('electric2').style.display = 'block';
+    } else if (type2 === "Fairy") {
+        document.getElementById('fairy2').style.display = 'block';
+    } else if (type2 === "Fighting") {
+        document.getElementById('fighting2').style.display = 'block';
+    } else if (type2 === "Fire") {
+        document.getElementById('fire2').style.display = 'block';
+    } else if (type2 === "Flying") {
+        document.getElementById('flying2').style.display = 'block';
+    } else if (type2 === "Ghost") {
+        document.getElementById('ghost2').style.display = 'block';
+    } else if (type2 === "Grass") {
+        document.getElementById('grass2').style.display = 'block';
+    } else if (type2 === "Ground") {
+        document.getElementById('ground2').style.display = 'block';
+    } else if (type2 === "Ice") {
+        document.getElementById('ice2').style.display = 'block';
+    } else if (type2 === "Poison") {
+        document.getElementById('poison2').style.display = 'block';
+    } else if (type2 === "Psychic") {
+        document.getElementById('psychic2').style.display = 'block';
+    } else if (type2 === "Rock") {
+        document.getElementById('rock2').style.display = 'block';
+    } else if (type2 === "Steel") {
+        document.getElementById('steel2').style.display = 'block';
+    } else if (type2 === "Water") {
+        document.getElementById('water2').style.display= 'block';
+    } 
+
 }
 
 //Declaring Pokemon types
 const pokemonType = () => {
 
-    let type1; 
+    
     let arrayPos = Math.floor(Math.random() * 18);
     type1 = pokemonType1[arrayPos];
     document.getElementById('type1').innerHTML = type1;
     secondType(type1);
 
+
+    //Pairing Type Symbol with Type
+    if (type1 === "Normal") {
+        document.getElementById('normal1').style.display = 'block';
+    } else if (type1 === "Bug") {
+        document.getElementById('bug1').style.display = 'block';
+    } else if (type1 === "Dark") {
+        document.getElementById('dark1').style.display = 'block';
+    } else if (type1 === "Dragon") {
+        document.getElementById('dragon1').style.display = 'block';
+    } else if (type1 === "Electricity") {
+        document.getElementById('electric1').style.display = 'block';
+    } else if (type1 === "Fairy") {
+        document.getElementById('fairy1').style.display = 'block';
+    } else if (type1 === "Fighting") {
+        document.getElementById('fighting1').style.display = 'block';
+    } else if (type1 === "Fire") {
+        document.getElementById('fire1').style.display = 'block';
+    } else if (type1 === "Flying") {
+        document.getElementById('flying1').style.display = 'block';
+    } else if (type1 === "Ghost") {
+        document.getElementById('ghost1').style.display = 'block';
+    } else if (type1 === "Grass") {
+        document.getElementById('grass1').style.display = 'block';
+    } else if (type1 === "Ground") {
+        document.getElementById('ground1').style.display = 'block';
+    } else if (type1 === "Ice") {
+        document.getElementById('ice1').style.display = 'block';
+    } else if (type1 === "Poison") {
+        document.getElementById('poison1').style.display = 'block';
+    } else if (type1 === "Psychic") {
+        document.getElementById('psychic1').style.display = 'block';
+    } else if (type1 === "Rock") {
+        document.getElementById('rock1').style.display = 'block';
+    } else if (type1 === "Steel") {
+        document.getElementById('steel1').style.display = 'block';
+    } else if (type1 === "Water") {
+        document.getElementById('water1').style.display= 'block';
+    }
+
+
 }
+
+
 
 //Master function
 const masterFunction = () => {
     pokemonFactory();
+    resetPicture();
     pokemonType();
 }
+
+
